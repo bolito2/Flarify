f802983eccc4439d7ae35c17d9b3d68f12fa2419
Bolito2 <bolito2hd@gmail.com>
2018-03-26 17:04:47 +0200

    Añadido soporte a los brillos que se salen de la pantalla

--- EyeFlare/main.cpp
+++ EyeFlare/main.cpp
@@ -11,17 +11,31 @@ using namespace std;
 using namespace cv;
 
 float multiplier = 8;
-bool showDebug = true;
+bool showDebug = false;
+
+float constantSize = 0.03f;
 
 int main(int argc, char** argv) {
-	Mat img = imread(argv[1], IMREAD_COLOR);
+	if (argc > 3) {
+		cout << "Error: too many arguments." << endl;
+		return 69;
+	}
+	if (argc == 3 && strcmp(argv[1], "-d") == 0)showDebug = true;
+
+	Mat img = imread(argc == 3 ? argv[2] : argv[1], IMREAD_COLOR);
+	if (img.empty()){
+		cout << "No existe la foto que intentas abrir." << endl;
+		return 420;
+	}
+	cout << (img.rows + img.cols) / 2 << endl;
 	Mat gris;
 	cvtColor(img, gris, COLOR_BGR2GRAY);
+	equalizeHist(gris, gris);
 
 	CascadeClassifier eyes_classifier("haarcascade_eye.xml");
 	vector<Rect> eyes;
 
-	eyes_classifier.detectMultiScale(gris, eyes, 1.3, 5);
+	eyes_classifier.detectMultiScale(gris, eyes, 1.1, 4, 0, Size(25,25), Size(150,150));
 	int maxEyeSize = 0;
 	Mat debug;
 	img.copyTo(debug);
@@ -62,21 +76,31 @@ int main(int argc, char** argv) {
 			float eyeCenterX = eyes[i].x + eyes[i].width / 2;
 			float eyeCenterY = eyes[i].y + eyes[i].height / 2;
 
-			Mat roi = img(Rect(eyeCenterX - flareCenterX, eyeCenterY - flareCenterY, flareWidth, flareHeight));
+			Rect img_rect = Rect(0,0,img.cols, img.rows);
+			Rect roi_rect = Rect(eyeCenterX - flareCenterX, eyeCenterY - flareCenterY, flareWidth, flareHeight);
+			Rect roi_rect_bounded = roi_rect & img_rect;
+			Rect flare_roi_rect = Rect(0,0,roi_rect.width, roi_rect.height) & Rect(roi_rect_bounded.x - (eyeCenterX - flareCenterX), roi_rect_bounded.y - (eyeCenterY - flareCenterY), roi_rect_bounded.width, roi_rect_bounded.height);
 
-			multiply(Scalar::all(1.0) - flare_alpha, roi, roi);
+			Mat flare_roi = flare(flare_roi_rect);
+			Mat flare_alpha_roi = flare_alpha(flare_roi_rect);
+			Mat roi = img(roi_rect_bounded);
 
-			add(flare, roi, roi);
+			//imshow(to_string(i) + "-flare_alpha : ", flare_alpha_roi);
+			//imshow(to_string(i) + "-roi : ", roi);
+			cout << eyes[i].width << endl;
+			multiply(Scalar::all(1.0) - flare_alpha_roi, roi, roi);
+
+			add(flare_roi, roi, roi);
 		}
 
 	}
+	
 	img.convertTo(img, -1, 2, 0);
-	char* heces = "_estallado.png";
-	strcat(argv[1], heces);
 	imshow("superposicion", img);
 	
 	img.convertTo(img, CV_8UC3, 255.0);
-	imwrite(argv[1], img);
+	imwrite((argc == 3 ? string(argv[2]) : string(argv[1])) + "_estallado.png", img);
+	
 
 	waitKey(0);
 	destroyAllWindows();
